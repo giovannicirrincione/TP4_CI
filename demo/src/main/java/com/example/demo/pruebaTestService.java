f03aae557e04cbed8c57a82c364cc361fa25b0ef
giovannicirrincione <giovannicirrincione22@gmail.com>
2024-06-05 14:17:33 -0300

    Add GitHub Actions CI workflow

---
+++ demo/src/main/java/com/example/demo/pruebaTestService.java
@@ -0,0 +1,10 @@
+package com.example.demo;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class pruebaTestService {
+    public int sumar (int a, int b){
+        return a+b;
+    }
+}
---
+++ demo/src/test/java/com/example/demo/pruebaTest.java
@@ -0,0 +1,26 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest
+public class pruebaTest {
+    @Test
+    public void testSumar() {
+
+        int a = 5;
+        int b = 3;
+        int expectedResult = 8;
+        pruebaTestService pruebaService = new pruebaTestService();
+
+        // Act (actuar, llamar al método que quieres probar)
+        int result = pruebaService.sumar(a, b);
+
+        // Assert (verificar el resultado)
+        // Aquí puedes verificar si el resultado es el esperado
+        assertEquals(expectedResult, result);
+    }
+}
