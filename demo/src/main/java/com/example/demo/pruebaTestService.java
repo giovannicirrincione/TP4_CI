a9f20e1508569aac3460637329fbfb0d554a5ac3
giovannicirrincione <giovannicirrincione22@gmail.com>
2024-06-06 15:00:17 -0300

    testing service, controller  and repository

--- demo/src/main/java/com/example/demo/pruebaTestService.java
+++ demo/src/main/java/com/example/demo/pruebaTestService.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Service;
 @Service
 public class pruebaTestService {
     public int sumar (int a, int b){
+        System.out.println("hola");
         return a+b;
     }
 }
